7c34ea814b769ead0a59e77a51c5cb3a650cd845
dsy189legend <dsy189legend@gmail.com>
2012-11-28 14:58:26 +0800

    更改为pop方式弹回去

--- Delegate_show03/Delegate_show03/ViewControllerB.m
+++ Delegate_show03/Delegate_show03/ViewControllerB.m
@@ -37,7 +37,7 @@
     UIButton *btn =  [UIButton buttonWithType:UIButtonTypeRoundedRect];
     [btn setTitle:@"委托给A来打印" forState:UIControlStateNormal];
     btn.frame = CGRectMake(0,148, 200, 44);
-    [btn addTarget:self action:@selector(jp2next) forControlEvents:UIControlEventTouchUpInside];
+    [btn addTarget:self action:@selector(transvalue) forControlEvents:UIControlEventTouchUpInside];
     
     UITextField *text = [[UITextField alloc] initWithFrame:CGRectMake(0, 100, 200, 44)];
     text.tag = 1001;
@@ -61,13 +61,19 @@
 }
 
 
--(void)jp2next{
+-(void)transvalue{
+    //上个页面跳过来的时候设置了指针委托，那么这里就会识别到delegate响应了这个方法；
     if ([delegate respondsToSelector:@selector(showName:)])
     {
         
         UITextField *mytext = (UITextField *)[self.view viewWithTag:1001];
         
+        
+        //step01
         [delegate showName:mytext.text];
+        
+        
+        [self.navigationController popToRootViewControllerAnimated:YES];
     }
 }
 
