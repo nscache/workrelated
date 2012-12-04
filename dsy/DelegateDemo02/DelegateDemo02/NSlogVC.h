d24908f59c003ea5407c79c5dd499122f0b9a4a0
dsy189legend <dsy189legend@gmail.com>
2012-12-04 13:21:31 +0800

    helo

---
+++ dsy/DelegateDemo02/DelegateDemo02/NSlogVC.h
@@ -0,0 +1,13 @@
+//
+//  NSlogVC.h
+//  DelegateDemo02
+//
+//  Created by bravesoft on 12-11-28.
+//  Copyright (c) 2012年 bravesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSlogVC : UIViewController
+
+@end
